fa92f6e676aee21efe7d845a220db47e9a33be0f
matiasceccato <matiasceccato1@gmail.com>
2022-10-18 23:32:05 +0000

    lab 5

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -6,4 +6,38 @@ class MonstersController < ApplicationController
   def show
     @monster = Monster.find(params[:id])
   end
+
+  def new
+    @monster = Monster.new
+
+    render :new
+  end
+
+  def create
+    @monster = Monster.new(monster_params)
+    if @monster.save
+      redirect_to edit_monster_path(@monster)
+    else
+      render :new, status: :see_other
+    end
+  end
+
+  def edit
+    @monster = Monster.find(params[:id])
+
+    render :edit
+  end
+
+  def update
+    @monster = Monster.find(params[:id])
+    if @monster.update(monster_params)
+      redirect_to @monster
+    else
+      render :new, status: :see_other
+    end
+  end
+
+  def monster_params
+    params.require(:monster).permit(:name, :description, :phone, :birthdate)
+  end
 end
